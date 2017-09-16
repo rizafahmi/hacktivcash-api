2a4c5007ef2c3760d78ffe3d435ee051415639e0
Riza Fahmi <rizafahmi@gmail.com>
2017-09-16 09:28:28 +0700

    Update controllers to use mongoose

--- controllers/product.js
+++ controllers/product.js
@@ -1,34 +1,41 @@
-const _ = require('lodash')
+const Product = require('../models/product')
 
 module.exports = app => {
-  _products = []
-
   // Create
   app.post('/product', (req, res) => {
-    _products.push(req.body)
-    res.json({ info: 'product created successfully.' })
+    const newProduct = new Product(req.body)
+    newProduct
+      .save()
+      .then(result => {
+        res.json({ info: 'product created successfully.' })
+      })
+      .catch(err => console.error(err))
   })
+
   // Read
   app.get('/product', (req, res) => {
-    res.send(_products)
+    Product.find()
+      .then(products => {
+        res.json(products)
+      })
+      .catch(err => console.error(err))
   })
-  app.get('/product/:id', (req, res) => {
-    res.json(_.find(_products, { id: req.params.id }))
-  })
+
   // Update
   app.put('/product/:id', (req, res) => {
-    const index = _.findIndex(_products, {
-      id: parseInt(req.params.id)
-    })
-    _.merge(_products[index], req.body)
-    res.json({ info: 'product updated successfully' })
+    Product.update({ _id: req.params.id }, req.body)
+      .then(product => {
+        res.json({ info: 'product updated successfully' })
+      })
+      .catch(err => console.error(err))
   })
 
   // Delete
   app.delete('/product/:id', (req, res) => {
-    _.remove(_products, product => {
-      return product.id === parseInt(req.params.id)
-    })
-    res.json({ info: 'product removed successfully' })
+    Product.remove({ _id: req.params.id })
+      .then(() => {
+        res.json({ info: 'product removed successfully' })
+      })
+      .catch(err => console.error(err))
   })
 }
